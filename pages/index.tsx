807746a777de20a552768db0a087fe63140c4612
Ralph Largo <devlargs@gmail.com>
2020-09-20 13:30:58 +0800

    feature: redirect index page to /customers

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/customers");
+  }, [router]);
+
+  return <></>;
+};
